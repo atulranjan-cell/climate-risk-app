299b3b14381e2dd948b1f81309c57ee8a4cd7216
atulranjan-cell <atul.ranjan@gistimpact.com>
2026-01-02 23:24:40 +0530

    Update hazards.py

--- hazards.py
+++ hazards.py
@@ -274,8 +274,7 @@ def sample_nearest_wri(coll, point, use_baseline_bau30, year=None, scenario_name
             return {
                 'Water Stress': safe_float(props, f'{pref}{y_key}_ws_x_s'),
                 'Seasonal Variability': safe_float(props, f'{pref}{y_key}_sv_x_s'),
-                'Interannual Variability': safe_float(props, f'{pref}{y_key}_iv_x_s'),
-                'Drought Risk': None
+                'Interannual Variability': safe_float(props, f'{pref}{y_key}_iv_x_s')
             }
     except:
         return None
@@ -813,6 +812,7 @@ def run_for_point(lat: float, lon: float):
     return df_final
 
                                      
+
 
 
 
